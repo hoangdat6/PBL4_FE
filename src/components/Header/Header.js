b1b7a37549b11c153345ddc4a075469e816088fe
hoangdat123 <dathv2004@gmail.com>
2024-09-27 00:37:34 +0700

    update the room code popup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,11 @@ import React from 'react';
 import styles from './Header.module.scss';
 import {useDispatch} from "react-redux";
 import {showSidebar} from "../../store/slices/sidebarSlice";
+import useOverlay from "../../hooks/useOverlay/useOverlay";
+import RoomCodePopup from "../RoomCodePopup/RoomCodePopup";
 
 const Header = () => {
+    const { toggleOverlay, Overlay} = useOverlay();
     const dispatch = useDispatch();
 
     const handleOpenSidebar = () => {
@@ -12,6 +15,9 @@ const Header = () => {
 
     return (
         <header className={`${styles.c_header}`}>
+            <Overlay closeHidden={true}>
+                <RoomCodePopup/>
+            </Overlay>
             <div className={`${styles.c_header__container}`}>
                 <div
                     className={`${styles.c_header__menu_icon}`}
@@ -21,7 +27,9 @@ const Header = () => {
                 </div>
                 <div className={`${styles.c_header__actions}`}>
                     <div className={`${styles.c_header__action_item}`}>
-                        <a href="#!" className="caro_btn btn_primary">
+                        <a href="#!" className="caro_btn btn_primary"
+                           onClick={toggleOverlay}
+                        >
                             <i className="fa-regular fa-keyboard"></i>
                             Nhập code
                         </a>
